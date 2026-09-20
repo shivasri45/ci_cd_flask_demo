d25da9c93299802b088f2e4cf00262e38258aa12
shivasri45 <shivanshsri45@gmail.com>
2026-09-20 22:39:50 +0530

    changed another version

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return """
     <h1>Hello from KIET Flask CI/CD</h1>
-    <h2>Version 3</h2>
+    <h2>Version 10</h2>
     <p>Deployed using AWS CodePipeline</p>
     """
 
